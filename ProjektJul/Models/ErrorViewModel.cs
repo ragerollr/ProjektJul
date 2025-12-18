c66fecc20c582d04d75b5d2bad713ce99acc0a43
Felix M <felixmoback@live.se>
2025-12-18 14:23:19 +0100

    Add project files.

---
+++ ProjektJul/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjektJul.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
