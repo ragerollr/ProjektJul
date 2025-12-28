6a4cefb3d5aa060a0031c01ffa4654d9db876c43
dennispera <dennispera@hotmail.se>
2025-12-28 16:42:07 +0100

    .

--- Projekt.Data/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjektJul.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
