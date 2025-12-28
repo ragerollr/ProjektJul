bc1b1e7efc7001ecc72735f8a17bf0dd27812e18
dennispera <dennispera@hotmail.se>
2025-12-28 23:23:47 +0100

    .

--- ProjektJul/Controllers/HomeController.cs
+++ ProjektJul/Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace Projekt.Web.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
