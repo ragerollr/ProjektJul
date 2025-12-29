d561aea732a8dc8bf206a3236e62a76a8e70dbc7
Felix M <felixmoback@live.se>
2025-12-29 19:51:57 +0100

    jj

---
+++ ProjektJul/ViewModels/CvModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Projekt.Web.ViewModels
+{
+    public class CvModel
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string ProfileUrl { get; set; } = string.Empty;
+        public string AvatarUrl { get; set; } = string.Empty;
+    }
+}
