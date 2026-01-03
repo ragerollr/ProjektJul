9680e8f548be4ea658877de952f47d31443f8a51
Felix M <felixmoback@live.se>
2026-01-03 11:06:10 +0100

    asd

--- ProjektJul/ViewModels/CvViewModel.cs
+++ ProjektJul/ViewModels/CvViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class CvViewModel
     {
-        public string FullName { get; set; }
+        public string FullName { get; set; } 
         public string Email { get; set; }
         public string Address { get; set; }
 
