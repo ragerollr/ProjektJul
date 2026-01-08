63e83bbfb584b0cf697f497c694ff8aa77a54a26
elinmariaa <elinmariasundell@gmail.com>
2026-01-08 21:04:43 +0100

    dataAnnotations mm

--- Projekt.Data/Identity/ApplicationUser.cs
+++ Projekt.Data/Identity/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace Projekt.Data.Identity
     public class ApplicationUser : IdentityUser
     {
         public string? FullName { get; set; }
+        public string? Address { get; set; }
         public bool IsPrivate { get; set; }
 
     }
