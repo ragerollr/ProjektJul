2109ac0d37208431d5617aec4b7b2a6aa446d864
dennispera <dennispera@hotmail.se>
2026-01-12 20:21:18 +0100

    .

--- ProjektJul/ViewModels/UserCheckboxViewmodel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Projekt.Web.ViewModels
-{
-    public class UserCheckboxViewmodel
-    {
-        public string? UserId { get; set; }
-        public string? FullName { get; set; }
-        public bool IsSelected { get; set; }
-    }
-}
