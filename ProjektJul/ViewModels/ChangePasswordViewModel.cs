38b81f57d69b37659f5a3601b7fdc3b4de4ca145
elinmariaa <elinmariasundell@gmail.com>
2026-01-13 13:03:42 +0100

    validering lösen

--- ProjektJul/ViewModels/ChangePasswordViewModel.cs
+++ ProjektJul/ViewModels/ChangePasswordViewModel.cs
@@ -4,15 +4,16 @@ namespace Projekt.Web.ViewModels
 {
     public class ChangePasswordVm
     {
-        [Required]
+        [Required(ErrorMessage = "Nuvarande lösenord krävs")]
         [DataType(DataType.Password)]
         public string CurrentPassword { get; set; } = "";
 
-        [Required]
+        [Required(ErrorMessage = "Nytt lösenord krävs")]
         [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Lösenordet måste vara minst 6 tecken")]
         public string NewPassword { get; set; } = "";
 
-        [Required]
+        [Required(ErrorMessage = "Bekräfta det nya lösenordet")]
         [DataType(DataType.Password)]
         [Compare("NewPassword", ErrorMessage = "Lösenorden matchar inte")]
         public string ConfirmNewPassword { get; set; } = "";
